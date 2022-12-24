87b8dcef5c480f2ea4fb5e11212162ac65bade1a
MrGharbi <moaminegharbi@gmail.com>
2022-12-24 09:44:32 +0100

    Fix Second task

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(exported = false)
-@Repository
 public interface AuthorRepository extends JpaRepository<Author,Long>{
 
 }
