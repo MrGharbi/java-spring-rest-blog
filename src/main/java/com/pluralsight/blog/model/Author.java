77b2011e72c0bef74cb718338cf51c377b38f45a
MrGharbi <moaminegharbi@gmail.com>
2022-12-24 09:39:21 +0100

    Second task

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -18,11 +18,16 @@ public class Author {
     private Long id;
     private String firstname;
     private String lastname;
+    @JsonIgnore
     private String username;
+    @JsonIgnore
     private String password;
+    @OneToMany
+    private List<Post> posts;
 
     public Author() {
         super();
+        this.posts = new ArrayList<Post>();
     }
 
     public Author(String username, String firstname, String lastname, String password) {
@@ -69,6 +74,11 @@ public class Author {
         return password;
     }
 
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+
     @Override
     public boolean equals(Object obj) {
         Author inputAuthor = (Author)obj;
@@ -85,10 +95,10 @@ public class Author {
     }
 
     public List<Post> getPosts() {
-        return null;
+        return posts;
     }
 
     public void addPost(Post post) {
-        return;
+         posts.add(post);
     }
 }
